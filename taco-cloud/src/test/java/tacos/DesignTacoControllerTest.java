80acce2d83f001435a79cd7a2f63c5077c749bc8
Jeff Fox <luck7@live.cn>
2021-04-07 12:57:09 +0800

    Annotating the domain as entities

--- taco-cloud/src/test/java/tacos/DesignTacoControllerTest.java
+++ taco-cloud/src/test/java/tacos/DesignTacoControllerTest.java
@@ -1,15 +1,20 @@
 package tacos;
 
-import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,6 +23,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import tacos.Ingredient.Type;
+import tacos.data.IngredientRepository;
+import tacos.data.OrderRepository;
+import tacos.data.TacoRepository;
 import tacos.web.DesignTacoController;
 
 @RunWith(SpringRunner.class)
@@ -30,6 +38,15 @@ public class DesignTacoControllerTest {
 	private List<Ingredient> ingredients;
 
 	private Taco design;
+
+	@MockBean
+	private IngredientRepository ingredientRepository;
+
+	@MockBean
+	private TacoRepository designRepository;
+
+	@MockBean
+	private OrderRepository orderRepository;
 
 	@Before
 	public void setup() {
@@ -45,15 +62,28 @@ public class DesignTacoControllerTest {
 				new Ingredient("SLSA", "Salsa", Type.SAUCE), 
 				new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
 
+		when(ingredientRepository.findAll()).thenReturn(ingredients);
+
+		when(ingredientRepository.findById("FLTO"))
+				.thenReturn(Optional.of(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP)));
+		when(ingredientRepository.findById("GRBF"))
+				.thenReturn(Optional.of(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN)));
+		when(ingredientRepository.findById("CHED"))
+				.thenReturn(Optional.of(new Ingredient("CHED", "Cheddar", Type.CHEESE)));
+
 		design = new Taco();
 		design.setName("Test Taco");
-		design.setIngredients(Arrays.asList("FLTO", "GRBF", "CHED"));
+
+		design.setIngredients(Arrays.asList(
+				new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+				new Ingredient("GRBF", "Ground Beef", Type.PROTEIN), 
+				new Ingredient("CHED", "Cheddar", Type.CHEESE)));
+
 	}
 
 	@Test
 	public void testShowDesignForm() throws Exception {
-		mockMvc.perform(get("/design"))
-				.andExpect(status().isOk()).andExpect(view().name("design"))
+		mockMvc.perform(get("/design")).andExpect(status().isOk()).andExpect(view().name("design"))
 				.andExpect(model().attribute("wrap", ingredients.subList(0, 2)))
 				.andExpect(model().attribute("protein", ingredients.subList(2, 4)))
 				.andExpect(model().attribute("veggies", ingredients.subList(4, 6)))
@@ -63,10 +93,13 @@ public class DesignTacoControllerTest {
 
 	@Test
 	public void processDesign() throws Exception {
-		mockMvc.perform(post("/design").content("name=Test+Taco&ingredients=FLTO,GRBF,CHED")
+		when(designRepository.save(design)).thenReturn(design);
+
+		mockMvc.perform(post("/design")
+				.content("name=Test+Taco&ingredients=FLTO,GRBF,CHED")
 				.contentType(MediaType.APPLICATION_FORM_URLENCODED))
-				.andExpect(status().is3xxRedirection())
-				.andExpect(header().stringValues("Location", "/orders/current"));
+		.andExpect(status().is3xxRedirection())
+		.andExpect(header().stringValues("Location", "/orders/current"));
 	}
 
 }
