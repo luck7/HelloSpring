21bb831c4f0be3811ddc57fdf16732f556e0994c
Jeff Fox <luck7@live.cn>
2021-04-20 20:28:59 +0800

    Update Ingredient.java
    JPA使用枚举类型

--- taco-cloud/src/main/java/tacos/Ingredient.java
+++ taco-cloud/src/main/java/tacos/Ingredient.java
@@ -1,6 +1,8 @@
 package tacos;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 
 import lombok.AccessLevel;
@@ -17,6 +19,7 @@ public class Ingredient {
 	@Id
 	private final String id;
 	private final String name;
+	@Enumerated(EnumType.STRING)
 	private final Type type;
 
 	public static enum Type {
